5addf572c545c6da71dd7e9d768c3ef5512e16f4
Brandon22023 <128272301+Brandon22023@users.noreply.github.com>
2024-10-18 11:13:28 -0600

    por si se me olvida

--- Proyecto2/generador_html_css.f90
+++ Proyecto2/generador_html_css.f90
@@ -36,7 +36,8 @@ MODULE generador_mod
 
     SUBROUTINE escribir_html(ruta_html, ruta_css)
         CHARACTER(LEN=*), INTENT(IN) :: ruta_html, ruta_css
-        integer :: i
+        integer :: i, j, k, l, m, n, o, p
+        integer :: largo
 
         OPEN(UNIT=10, FILE=ruta_html, STATUS='REPLACE')
         WRITE(10, '(A)') '<html>'
@@ -51,13 +52,43 @@ MODULE generador_mod
         ! Cuerpo principal que contiene otros elementos
         WRITE(10, '(A)') '        <div id="ContBody" style="width: 800px; height: 300px; background-color: rgb(64,224,208); position: absolute; left: 23px; top: 21px;">'
         
+
+       
+        
         ! Login container y sus elementos
         WRITE(10, '(A)') '            <div id="contlogin" style="width: 270px; height: 150px; background-color: rgb(47,79,79); position: absolute; left: 586px; top: 110px;">'
-        WRITE(10, '(A)') '                <label id="Nombre" style="width: 44px; height: 13px; color: rgb(128,128,128); position: absolute; left: 8px; top: 21px;">Nombre</label>'
-        WRITE(10, '(A)') '                <input type="text" id="Texto0" style="position: absolute; left: 65px; top: 20px;" />'
-        WRITE(10, '(A)') '                <label id="passw" style="width: 53px; height: 13px; color: rgb(128,128,128); position: absolute; left: 11px; top: 54px;">Password</label>'
-        WRITE(10, '(A)') '                <input type="password" id="pswClave" style="position: absolute; left: 67px; top: 48px;" />'
-        WRITE(10, '(A)') '                <button id="cmdIngresar" style="position: absolute; left: 40px; top: 100px;">Ingresar</button>'
+        do i = 1, size(contenido_add_array)
+            ! Imprimir los valores que se están comparando
+            print *, "Comparando ID: ", trim(contenido_add_array(i)%id), " con 'this'"
+            print *, "Comparando ADD: ", trim(contenido_add_array(i)%add), " con 'contlogin'"
+            ! Asegúrate de eliminar espacios en ambas comparaciones
+            if (trim(contenido_add_array(i)%id) == "ContBody" .and. trim(contenido_add_array(i)%add) == "contlogin") then
+            
+
+                do j = 1, size(contenido_add_array)
+                    
+                    do k = 1, size(etiqueta_array)
+                        if (trim(etiqueta_array(k)%id) == trim(contenido_add_array(j)%idd)) then
+                            if (trim(contenido_add_array(j)%id) == "contlogin" .and. trim(contenido_add_array(j)%add) == trim(etiqueta_array(k)%id)) then
+                                WRITE(10, '(A)') '                <label id="'// trim(contenido_add_array(j)%id) //'" style="width: 44px; height: 13px; color: rgb(128,128,128); position: absolute; left: 8px; top: 21px;">Nombre</label>'
+                                WRITE(10, '(A)') '                <input type="text" id="Texto0" style="position: absolute; left: 65px; top: 20px;" />'
+                            end if
+                            
+                            if (trim(contenido_add_array(j)%id) == "contlogin" .and. trim(contenido_add_array(j)%add) == "passw") then
+                                WRITE(10, '(A)') '                <label id="'// trim(contenido_add_array(j)%id) //'" style="width: 53px; height: 13px; color: rgb(128,128,128); position: absolute; left: 11px; top: 54px;">Password</label>'
+                                WRITE(10, '(A)') '                <input type="password" id="pswClave" style="position: absolute; left: 67px; top: 48px;" />'
+                            end if
+                        end if
+                        
+                    end do
+                    if (trim(contenido_add_array(j)%id) == "contlogin" .and. trim(contenido_add_array(j)%add) == "cmdIngresar") then
+                        print *, "comparación correcta para cmdIngresar"
+                        WRITE(10, '(A)') '                <button id="'// trim(contenido_add_array(j)%id) //'" style="position: absolute; left: 40px; top: 100px;">Ingresar</button>'
+                    end if
+                end do
+            end if
+        end do
+
         WRITE(10, '(A)') '            </div>'
 
         ! Logo containers
@@ -76,11 +107,6 @@ MODULE generador_mod
 
         OPEN(UNIT=11, FILE=ruta, STATUS='REPLACE')
         WRITE(11, '(A)') '/* Estilos generales */'
-        WRITE(11, '(A)') 'body {'
-        WRITE(11, '(A)') '    font-family: Arial, sans-serif;'
-        WRITE(11, '(A)') '    margin: 0;'
-        WRITE(11, '(A)') '    padding: 0;'
-        WRITE(11, '(A)') '}'
         WRITE(11, '(A)') '/* Estilos para contFondo */'
         WRITE(11, '(A)') '#contFondo {'
         WRITE(11, '(A)') '    background-color: rgb(64, 64, 64);'
@@ -91,6 +117,7 @@ MODULE generador_mod
         WRITE(11, '(A)') '}'
         WRITE(11, '(A)') '#Nombre {'
         WRITE(11, '(A)') '    color: rgb(128, 128, 128);'
+        WRITE(11, '(A)') '}'
         WRITE(11, '(A)') '#passw {'
         WRITE(11, '(A)') '    color: rgb(128, 128, 128);'
         WRITE(11, '(A)') '}'
